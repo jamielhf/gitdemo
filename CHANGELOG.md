afcef77f1b6600255241c916216ba393a5e2f68a
linhaifeng <linhaifeng@yy.com>
2018-10-10 23:44:14 +0800

    fix: 修改

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,9 @@
 # 1.0.0 (2018-10-10)
 
 
+### Features
+
+* 新增showAge方法 ([35de4fa](https://github.com/jamielhf/gitdemo/commit/35de4fa))
+
+
 
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const name = 1;
+const age = 17;
 
 function showName() {
   console.log(name);
+}
+
+function showAge() {
+  console.log(age);
 }
